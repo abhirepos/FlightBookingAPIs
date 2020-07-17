0066901ef254b98024119f17d6ce7ad03c2676c6
Subham_Cenrax <43017632+Cenrax@users.noreply.github.com>
2020-07-17 22:59:08 +0530

    1st_Commit_BookFlight_SingleFlight
    I did it on considering single flight but I am working on other API and writing the code for business logic for considering for multiple flights.

---
+++ BusinessLogic/BookFlight.cs
@@ -0,0 +1,66 @@
+using Microsoft;
+using System;
+
+namespace FlightBookingAPIs.BusinessLogic
+{
+    public class BusinessRules_FlightBooking
+    {   int MaxSeats = 50;
+        
+        public int payment(String PaymentStatus, int available )
+        {
+            if (available<0)
+                return -1;
+                
+            else 
+            {
+                if (PaymentStatus == "Paid")
+                {
+                    
+                    available = available - 1;
+                    return 1;
+                }
+                else
+                {
+                    
+                    return 0;
+                }
+            }
+        }
+
+        
+
+        public static void main(String args)
+        {
+            String [] CusID = new String[]{"A001","A002","A003"};
+            String [] PaymentDemo = new String[]{"Paid","Pending","Paid"};
+            BusinessRules_FlightBooking ob = new BusinessRules_FlightBooking();
+            String [] Confirmed= new String[3];
+            int c =0;
+            int available = 6;
+            for (int count = 0 ; count<= CusID.Length; count++)
+            {
+                if(ob.payment(PaymentDemo[count],available)==-1)
+                    Console.WriteLine("Cannot Confirm the booking due to unavailability of seats");
+                else if(ob.payment(PaymentDemo[count],available)==1)
+                {
+                    Console.WriteLine("Confirmed");
+                    Confirmed[c++]=CusID[count];
+                    available = available-1;
+                }
+                else
+                {
+                    Console.WriteLine("Pending");
+                }
+            }
+            
+
+        }
+        
+    }
+    
+        
+        
+
+
+    
+}
