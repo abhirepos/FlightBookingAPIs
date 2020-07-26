5d29aa2d5e3b0575e5245cca0c447c48d58b0c91
Subham_Cenrax <43017632+Cenrax@users.noreply.github.com>
2020-07-26 09:22:20 +0530

    Update BookFlight.cs

--- BusinessLogic/BookFlight.cs
+++ BusinessLogic/BookFlight.cs
@@ -1,66 +1,27 @@
-using Microsoft;
 using System;
-
-namespace FlightBookingAPIs.BusinessLogic
-{
-    public class BusinessRules_FlightBooking
-    {  
-        
-        public int payment(String PaymentStatus, int available )
-        {
-            if (available<0)
+using System.Collections.Generic;
+namespace FlightBookingAPIs.BusinessLogic{
+    public class BookFlight{
+        public enum payStatus{
+            paid, deniedByBank, pending, cancelled
+        }
+        public int book (int id, int flight_no, int paymentStatus){
+            FlightAvailability ob=new FlightAvailability();
+            int value=ob.results(flight_no);
+            if (value==0){
                 return -1;
-                
-            else 
-            {
-                if (PaymentStatus == "Paid")
-                {
-                    
-                    available = available - 1;
-                    return 1;
+            }
+            else{
+                if(paymentStatus==(int)payStatus.paid && value>0){
+                    return value;
                 }
-                else
-                {
-                    
+                else if(paymentStatus!=(int)payStatus.cancelled && value>0){
+                    return -2;
+                }
+                else{
                     return 0;
                 }
             }
         }
-
-        
-
-        public static void main(String args)
-        {
-            String [] CusID = new String[]{"A001","A002","A003"};
-            String [] PaymentDemo = new String[]{"Paid","Pending","Paid"};
-            BusinessRules_FlightBooking ob = new BusinessRules_FlightBooking();
-            String [] Confirmed= new String[3];
-            int c =0;
-            int available = 6;
-            for (int count = 0 ; count<= CusID.Length; count++)
-            {
-                if(ob.payment(PaymentDemo[count],available)==-1)
-                    Console.WriteLine("Cannot Confirm the booking due to unavailability of seats");
-                else if(ob.payment(PaymentDemo[count],available)==1)
-                {
-                    Console.WriteLine("Confirmed");
-                    Confirmed[c++]=CusID[count];
-                    available = available-1;
-                }
-                else
-                {
-                    Console.WriteLine("Pending");
-                }
-            }
-            
-
-        }
-        
     }
-    
-        
-        
-
-
-    
 }
